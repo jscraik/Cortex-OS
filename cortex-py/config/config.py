a1ba756e34159c7ff35d6309672dba5c54d099d0
Jamie Scott Craik <154467285+jamiescottcraik@users.noreply.github.com>
2025-09-01 04:59:30 +0100

    feat(pipelines): vendor open-webui examples

---
+++ cortex-py/config/config.py
@@ -0,0 +1,29 @@
+# Copyright (c) Open WebUI
+# SPDX-License-Identifier: MIT
+# Ported to Cortex-OS (c) 2025 Cortex-OS Contributors
+
+
+import os
+import logging
+####################################
+# Load .env file
+####################################
+
+try:
+    from dotenv import load_dotenv, find_dotenv
+
+    load_dotenv(find_dotenv("./.env"))
+except ImportError:
+    print("dotenv not installed, skipping...")
+
+# Define log levels dictionary
+LOG_LEVELS = {
+    'DEBUG': logging.DEBUG,
+    'INFO': logging.INFO,
+    'WARNING': logging.WARNING,
+    'ERROR': logging.ERROR,
+    'CRITICAL': logging.CRITICAL
+}
+
+API_KEY = os.getenv("PIPELINES_API_KEY", "0p3n-w3bu!")
+PIPELINES_DIR = os.getenv("PIPELINES_DIR", "./pipelines")
