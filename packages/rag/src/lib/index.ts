282fd7cd00d42da374f1f94cb882e68f01bd604d
Jamie Scott Craik <154467285+jamiescottcraik@users.noreply.github.com>
2025-09-08 00:47:37 +0100

    test(rag): add citation bundler coverage

--- packages/rag/src/lib/index.ts
+++ packages/rag/src/lib/index.ts
@@ -1,7 +1,8 @@
-export * from "./embed-query.js";
-export * from "./generate-answer.js";
-export * from "./rerank-docs.js";
-export * from "./retrieve-docs.js";
-export * from "./run-process.js";
-export * from "./types.js";
-export type { Embedder, Chunk, Store, Pipeline } from "./types.js";
+export * from './citation-bundler.js';
+export * from './embed-query.js';
+export * from './generate-answer.js';
+export * from './rerank-docs.js';
+export * from './retrieve-docs.js';
+export * from './run-process.js';
+export type { Chunk, Citation, CitationBundle, Embedder, Pipeline, Store } from './types.js';
+export * from './types.js';
